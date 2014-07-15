eb83341ecb471d03141823855505950e569f094c
Zafar Takhirov <z.tahirov@gmail.com>
2014-07-14 23:05:46 -0700

    AUTOCOMMIT: Mon Jul 14 23:05:46 PDT 2014

--- Ch04/ch4.2_strassen.cpp
+++ Ch04/ch4.2_strassen.cpp
@@ -1,69 +1,22 @@
+#include <cassert>
 #include <cmath>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int> matrixCreate (int N) {
-	vector <int> matrix;
-	int size = N*N;
-	
-	matrix.resize(size);
-	return matrix;	
-}
-
-void matrixSet (vector<int> *matrix, int size, int row, int col, int val) {
-	int index = col + row*size;
-	matrix->at(index) = val;
-}
-
-int matrixGet (vector<int> matrix, int size, int row, int col) {
-	int index = col + row*size;
-	return (matrix[index]);
-}
-
-vector<int> squareMatrixMultiply (vector<int> A, vector<int> B, int size) {
-	int a, b, c, n;
-	n = sqrt(size);
-	vector<int> C = matrixCreate(size);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			matrixSet(&C, size, i, j, 0);
-			for (int k = 0; k < n; k++) {
-				a = matrixGet(A, size, i, j);
-				b = matrixGet(B, size, i, j);
-				c = matrixGet(C, size, i, j);
-				matrixSet(&C, size, i, j, a*b+c);
-			}
-		}
-	}
-	return C;
-}
+#include "matrixFunctions.h"
 
 int main() {
-	int N;
+	int N;	// N is the edge of a square matrix
+	cout << "Enter the number of rows/columns of a square matrix: ";
 	cin >> N;
-		
-	vector<int> A = matrixCreate(N);
-	vector<int> B = matrixCreate(N);
-	vector<int> C = matrixCreate(N);
 	
-	int num;
-	for (int i = 0; i < 4; i++) {
-		cin >> num;
-		A.push_back(num);
-	}
-	for (int i = 0; i < 4; i++) {
-		cin >> num;
-		B.push_back(num);
-	}
+	MATRIX A = matrixPopulate(N);
+	MATRIX B = matrixPopulate(N);
 	
-	C = squareMatrixMultiply(A, B, N*N);
+	MATRIX C = squareMatrixMultiply(A, B, N);
 	
-	for (vector<int>::iterator it = C.begin(); it != C.end(); ++it) {
-		cout << *it << ' ';
-	}
 	cout << endl;
-	
-	
+	matrixPrint(C, N);
 }
---
+++ Ch04/matrixFunctions.h
@@ -0,0 +1,98 @@
+typedef	std::vector<int>	MATRIX;
+
+// Returns an empty matrix of size NxN
+MATRIX matrixCreate (int N) {	// N is the edge of a square matrix
+	MATRIX matrix;
+	int size = pow(N, 2);
+	
+	matrix.resize(size);
+	return matrix;	
+}
+
+// Sets "val" to matrix(row, col)
+void matrixSet (MATRIX *matrix, int N, int row, int col, int val) {
+	assert (row < N && col < N);
+	// row and column indexing starts at 0
+	// N is the edge of the square matrix
+	int index = row * N + col;
+	matrix->at(index) = val;
+}
+
+// Gets the value located at matrix(row, col)
+int matrixGet (MATRIX matrix, int N, int row, int col) {
+	int index = row * N + col;
+	return matrix.at(index);
+}
+
+// Creates a zero matrix of size NxN
+MATRIX matrixZero(int N) {
+	MATRIX matrix = matrixCreate(N);
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < N; col++) {
+			matrixSet(&matrix, N, row, col, 0);
+		}
+	}
+	return matrix;
+}
+
+// Creates an identity matrix of size NxN
+MATRIX matrixEigen(int N) {
+	MATRIX matrix = matrixCreate(N);
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < N; col++) {
+			if (row == col)
+				matrixSet(&matrix, N, row, col, 1);
+			else
+				matrixSet(&matrix, N, row, col, 0);
+		}
+	}
+	return matrix;
+}
+
+MATRIX matrixPopulate(int N) {
+	MATRIX A = matrixCreate(N);
+	int num;
+	std::cout << "\nPlease enter the matrix row-by-row:\n";
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < N; col++) {
+			cin >> num;
+			matrixSet(&A, N, row, col, num);
+		}
+	}
+	return A;
+}
+
+// Shows the values in a matrix
+void matrixPrint (MATRIX matrix, int N) {	// N is the edge of a square matrix
+	for (int row = 0; row < N; row++) {		// Row
+		for (int col = 0; col < N; col++) {	// Col
+			std::cout << matrixGet(matrix, N, row, col) << '\t';
+		}
+		std::cout << endl;
+	}
+}
+
+// Matrix multiplication:
+MATRIX squareMatrixMultiply (MATRIX A, MATRIX B, int N) {
+	/*** BEGIN ALGORITHM 
+	n = A.rows
+	let C be a new nxn matrix
+	for i = 1 to n
+		for j = 1 to n
+			c_ij = 0
+			for k = 1 to n
+				c_ij = c_ij + a_ik * b_kj
+	return C
+	END ALGORITHM ***/
+	int val;
+	MATRIX C = matrixZero(N);
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < N; col++) {
+			for (int k = 0; k < N; k++) {
+				val = matrixGet(C, N, row, col) + matrixGet(A, N, row, k) * matrixGet(B, N, k, col);
+				matrixSet(&C, N, row, col, val);
+			}
+		}
+	}
+	return C;
+}
