381d590298285d7cff417dfeb5a1f8cf93d68fe2
Zafar Takhirov <z.tahirov@gmail.com>
2014-07-16 23:18:41 -0700

    AUTOCOMMIT: Wed Jul 16 23:18:41 PDT 2014

---
+++ support/myFunctions.hpp
@@ -0,0 +1,19 @@
+#ifndef __MY_FUNCTIONS__
+#define __MY_FUNCTIONS__
+
+
+
+void vectorPrint (vector<int> A) {
+  for (vector<int>::iterator it = A.begin(); it != A.end(); ++it)
+    cout << *it << ' ';
+  cout << endl;
+}
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+
+#endif
