0a0ba6c7e5f1b1b26592b7eaa51d34f640e3549d
Zafar Takhirov <z.tahirov@gmail.com>
2014-07-24 15:23:01 -0700

    finished the interval sorting

--- Ch07/p7-6.cpp
+++ Ch07/p7-6.cpp
@@ -42,10 +42,40 @@ bool intervalEQ (interval A, interval B) {
   return ((A.min == B.min) && (A.max == B.max));
 }
 
+void swap (interval *A, interval *B) {
+  interval temp = *A;
+  *A = *B;
+  *B = temp;
+}
+
+int partition(vector<interval> *A, int p, int r) {
+  interval x = A->at(r);
+  int i = p - 1;
+  for (int j = p; j <= r-1; j++) {
+    if (intervalLT(A->at(j), x) || intervalEQ(A->at(j), x)) {
+      i++;
+      swap(A->at(i), A->at(j));
+    }
+  }
+  swap(A->at(i+1), A->at(r));
+  return i+1;
+}
+
+void quickSort(vector<interval> *A, int p, int r) {
+  int q;
+  if (p < r) {
+    q = partition (A, p, r);
+    quickSort(A, p, q-1);
+    quickSort(A, q+1, r);
+  }
+}
+
 int main() {
-  interval ar[5] = {{1,2}, {0, 1}, {10, 20}, {21,22}, {5,7}};
+  interval ar[5] = {{1, 3}, {0, 2}, {10, 20}, {21,22}, {5,7}};
   vector<interval> A;
   A.insert(A.begin(), ar, ar+5);
 
+  intervalPrint(A);
+  quickSort(&A, 0, A.size()-1);
   intervalPrint(A);
 }
