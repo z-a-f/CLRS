ee07517e95307d08019a497399816f9e26459ff2
Zafar Takhirov <z.tahirov@gmail.com>
2014-07-24 22:40:31 -0700

    AUTOCOMMIT: Thu Jul 24 22:40:31 PDT 2014

--- Ch08/countingSort.cpp
+++ Ch08/countingSort.cpp
@@ -1,44 +1,16 @@
-#include <cmath>
-#include <iostream>
-#include <vector>
+#include "../support/myIncludes.hpp"
 
 using namespace std;
 
-void printVector (vector<int> A) {
-  for (vector<int>::iterator it = A.begin(); it != A.end(); ++it)
-    cout << *it << ' ';
-  cout << endl;
-}
-
-vector<int> countingSort(vector<int> A, int k) {
-    vector<int> B = A;  // Just initialize to anything for now;
-    int C[k+1];
-    for (int i = 0; i <= k; i++) {
-        C[i] = 0;
-    }
-    for (vector<int>::iterator it = A.begin(); it != A.end(); ++it) {
-        C[*it]++;
-    }
-    for (int i = 1; i <= k; i++) {
-        C[i] += C[i-1];
-    }
-    for (vector<int>::iterator it = A.end(); it != A.begin(); --it) {
-        B.at(C[*it]) = *it;
-        C[*it] -= 1;
-    }
-    
-    return B;
-}
+#include "../support/myFunctions.hpp"
+#include "./countingSort.hpp"
 
 int main() {
-    vector<int> A;
-    int N, k, num;
-    cin >> N >> k;
-    for (int i = 0; i < N; i++) {
-        cin >> num;
-        A.push_back(num);
-    }
-    printVector(A);
-    A = countingSort(A, k);
-    printVector(A);
+  int ar[15] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5};
+  vector<int> A;
+  A.insert(A.begin(), ar, ar+15);
+
+  printVector(A);
+  countingSort(&A, 10);
+  printVector(A);
 }
